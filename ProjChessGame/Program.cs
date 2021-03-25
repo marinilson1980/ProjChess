3f4636189f3d77ba31e112390336598542b417f7
marinilson <79950142+marinilson1980@users.noreply.github.com>
2021-03-24 22:29:17 -0300

    Comecando implementar o tabuleiro

--- ProjChessGame/Program.cs
+++ ProjChessGame/Program.cs
@@ -5,11 +5,8 @@ namespace ProjChessGame {
     class Program {
         static void Main(string[] args)
         {
-            Posicao p;
-
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.ReadLine();
         }
     }
 }
