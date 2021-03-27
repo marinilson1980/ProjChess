ee80568915aae5fe44a00d9f3990c5c9b5a18101
marinilson <79950142+marinilson1980@users.noreply.github.com>
2021-03-27 12:22:56 -0300

    Posicao xadrez

---
+++ ProjChessGame/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using tabuleiro;
+
+namespace Chess {
+    class ChessPosition {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public ChessPosition(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha; 
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+    }
+}
--- ProjChessGame/Program.cs
+++ ProjChessGame/Program.cs
@@ -6,7 +6,7 @@ namespace ProjChessGame {
     class Program {
         static void Main(string[] args)
         {
-
+            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
@@ -14,7 +14,6 @@ namespace ProjChessGame {
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(-2, 4));
 
                 Tela.imprimirTabuleiro(tab);
                 
@@ -23,7 +22,13 @@ namespace ProjChessGame {
             {
                 Console.WriteLine(e.Message); ;
             }
+            Console.ReadLine();*/
+
+            ChessPosition pos = new ChessPosition('a', 1);
+            Console.WriteLine(pos.toPosicao());
+            Console.WriteLine(pos);
             Console.ReadLine();
+
         }
     }
 }
