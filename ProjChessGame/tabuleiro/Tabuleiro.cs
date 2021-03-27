64783bde9e88da5952dc3962416d9f96675bdb34
marinilson <79950142+marinilson1980@users.noreply.github.com>
2021-03-27 11:40:06 -0300

    Comecando a colocar pecas no tabuleiro

--- ProjChessGame/tabuleiro/Tabuleiro.cs
+++ ProjChessGame/tabuleiro/Tabuleiro.cs
@@ -1,4 +1,7 @@
-﻿namespace tabuleiro {
+﻿using tabuleiro;
+
+
+namespace tabuleiro {
     class Tabuleiro {
         public int linhas { get; set; }
         public int colunas { get; set; }
@@ -9,6 +12,16 @@
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
         }
     }
 }
