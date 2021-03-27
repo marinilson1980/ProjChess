c9eac84c7ef8a88260eb53c58b57a5a41eb645e4
marinilson <79950142+marinilson1980@users.noreply.github.com>
2021-03-27 12:32:46 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- ProjChessGame/Program.cs
+++ ProjChessGame/Program.cs
@@ -6,7 +6,7 @@ namespace ProjChessGame {
     class Program {
         static void Main(string[] args)
         {
-            /*
+
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
@@ -15,6 +15,9 @@ namespace ProjChessGame {
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+
                 Tela.imprimirTabuleiro(tab);
                 
             }
@@ -22,13 +25,7 @@ namespace ProjChessGame {
             {
                 Console.WriteLine(e.Message); ;
             }
-            Console.ReadLine();*/
-
-            ChessPosition pos = new ChessPosition('a', 1);
-            Console.WriteLine(pos.toPosicao());
-            Console.WriteLine(pos);
             Console.ReadLine();
-
         }
     }
 }
